ff8fdf0d6df4b3d144fd8b69bd43e5f2cb3fc326
Meow0824 <luna.gaming0824@gmail.com>
2026-06-23 13:29:56 +0000

    fix: use HashRouter for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 
 // GitHub Pages SPA redirect
@@ -14,10 +14,10 @@ import { AuthProvider } from './hooks/useAuth'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="">
+    <HashRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
